9cc14e55d860f5f982e6ade3a6cfe7d5fdb7ba98
adipati27ma <adipati.cs.samsung@gmail.com>
2024-01-24 16:23:39 +0700

    Polymorphism & method polymorphism

---
+++ tests/polymorphism.test.ts
@@ -0,0 +1,58 @@
+// Polymorphism = berubah bentuk tapi hanya bisa berubah menjadi class turunannya.
+// Method Polymorphism = method yg bisa diinput dgn class tersebut atau turunannya.
+describe('Polymorphism', () => {
+  class Employee {
+    constructor(public name: string) {}
+  }
+
+  class Manager extends Employee {}
+
+  class VicePresident extends Manager {}
+
+  function sayHello(employee: Employee): void {
+    if (employee instanceof VicePresident) {
+      const vp = employee as VicePresident;
+      console.info(`Hello VP ${vp.name}`);
+    } else if (employee instanceof Manager) {
+      const manager = employee as Manager;
+      console.info(`Hello manager ${manager.name}`);
+    } else {
+      console.info(`Hello employee ${employee.name}`);
+    }
+  }
+
+  function sayHelloWrong(employee: Employee): void {
+    if (employee instanceof Manager) {
+      const manager = employee as Manager;
+      console.info(`Hello manager ${manager.name}`);
+    } else if (employee instanceof VicePresident) {
+      const vp = employee as VicePresident;
+      console.info(`Hello VP ${vp.name}`);
+    } else {
+      console.info(`Hello employee ${employee.name}`);
+    }
+  }
+
+  it('should support', () => {
+    let employee: Employee = new Employee('Eko');
+    console.info(employee);
+
+    employee = new Manager('Eko');
+    console.info(employee);
+
+    employee = new VicePresident('Eko');
+    console.info(employee);
+  });
+
+  it('should support method parameter polymorphism', () => {
+    sayHello(new Employee('Eko'));
+    sayHello(new Manager('Budi'));
+    sayHello(new VicePresident('Joko'));
+  });
+
+  it('should support method parameter polymorphism wrong', () => {
+    sayHelloWrong(new Employee('Eko'));
+    sayHelloWrong(new Manager('Budi'));
+    sayHelloWrong(new VicePresident('Joko'));
+  });
+});
